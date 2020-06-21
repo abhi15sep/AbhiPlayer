3db610a836c51cd5828035b28d493783b4acb731
Abhay Pratap Singh <abhaypratap3536@gmail.com>
2020-06-21 03:02:55 +0100

    Made resizable false

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ function createWindow() {
         frame: false,
         maximizable: false,
         transparent: true,
+        resizable: false,
         webPreferences: {
             nodeIntegration: true,
         }
